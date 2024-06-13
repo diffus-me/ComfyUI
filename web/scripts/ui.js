4b6b8767a6c228b141055bc7e9966f895774e231
btian <ben.tian@graviti.com>
2024-06-13 14:16:35 +0800

    feat: disable settings button

--- web/scripts/ui.js
+++ web/scripts/ui.js
@@ -465,10 +465,6 @@ export class ComfyUI {
 				$el("span.drag-handle"),
 				$el("span.comfy-menu-queue-size", { $: (q) => (this.queueSize = q) }),
 				$el("div.comfy-menu-actions", [
-					$el("button.comfy-settings-btn", {
-						textContent: "⚙️",
-						onclick: () => this.settings.show(),
-					}),
 					$el("button.comfy-close-menu-btn", {
 						textContent: "\u00d7",
 						onclick: () => {
