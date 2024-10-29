be28962308eddfa54e59d56f857a5b37f395af75
btian <ben.tian@diffus.me>
2024-10-29 11:08:48 +0000

    new web html

--- web/scripts/ui.js
+++ web/scripts/ui.js
@@ -1,4 +1,5 @@
 // Shim for scripts/ui.ts
 export const ComfyDialog = window.comfyAPI.ui.ComfyDialog;
+export const ComfyConfirmDialog = window.comfyAPI.ui.ComfyConfirmDialog;
 export const $el = window.comfyAPI.ui.$el;
 export const ComfyUI = window.comfyAPI.ui.ComfyUI;
