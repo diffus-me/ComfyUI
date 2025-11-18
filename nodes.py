6463bc8b63eb5e1ec1083e959a83baf62e90864c
btian <ben.tian@graviti.cn>
2025-11-18 22:30:27 +0800

    feat: CLIPSetLastLayer to be optional

--- nodes.py
+++ nodes.py
@@ -650,15 +650,18 @@ class CLIPSetLastLayer:
     def INPUT_TYPES(s):
         return {"required": { "clip": ("CLIP", ),
                               "stop_at_clip_layer": ("INT", {"default": -1, "min": -24, "max": -1, "step": 1}),
-                              }}
+                              },
+                "optional": {"enable": ("BOOLEAN", {"default": True}),}
+            }
     RETURN_TYPES = ("CLIP",)
     FUNCTION = "set_last_layer"
 
     CATEGORY = "conditioning"
 
-    def set_last_layer(self, clip, stop_at_clip_layer):
-        clip = clip.clone()
-        clip.clip_layer(stop_at_clip_layer)
+    def set_last_layer(self, clip, stop_at_clip_layer, enable:bool = True):
+        if enable:
+            clip = clip.clone()
+            clip.clip_layer(stop_at_clip_layer)
         return (clip,)
 
 class LoraLoader:
