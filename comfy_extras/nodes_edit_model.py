f223b57a71615f20ed8067cc0ee32088bb415067
btian <ben.tian@graviti.cn>
2025-11-25 19:00:12 +0800

    feat: add option to disable ReferenceLatent node

--- comfy_extras/nodes_edit_model.py
+++ comfy_extras/nodes_edit_model.py
@@ -6,7 +6,9 @@ class ReferenceLatent:
     def INPUT_TYPES(s):
         return {"required": {"conditioning": ("CONDITIONING", ),
                             },
-                "optional": {"latent": ("LATENT", ),}
+                "optional": {
+                    "latent": ("LATENT", ),
+                    "enable": ("BOOLEAN", {"default": True}),}
                }
 
     RETURN_TYPES = ("CONDITIONING",)
@@ -15,8 +17,8 @@ class ReferenceLatent:
     CATEGORY = "advanced/conditioning/edit_models"
     DESCRIPTION = "This node sets the guiding latent for an edit model. If the model supports it you can chain multiple to set multiple reference images."
 
-    def append(self, conditioning, latent=None):
-        if latent is not None:
+    def append(self, conditioning, latent=None, enable: bool=True):
+        if enable and latent is not None:
             conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": [latent["samples"]]}, append=True)
         return (conditioning, )
 
