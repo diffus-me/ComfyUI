4f7d1088c37c282bc2335dc74a890b8ee1c4eaae
btian <tiangles@outlook.com>
2026-09-22 15:06:52 +0800

    feat: disable cleanup_temp_filesystem at startup

--- main.py
+++ main.py
@@ -588,8 +588,8 @@ def start_comfyui(asyncio_loop=None):
         folder_paths.set_temp_directory(temp_dir)
 
     asset_manager: AssetManager = default_asset_manager()
-    if not asset_manager.enabled:
-        cleanup_temp_filesystem(None)
+    # if not asset_manager.enabled:
+    #     cleanup_temp_filesystem(None)
 
     if not asyncio_loop:
         asyncio_loop = asyncio.new_event_loop()
